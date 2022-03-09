a5bbc81566f274fb3d2187ea7586a729d973e2eb
R289081 <R289081@VW10A26923.principalusa.corp.principal.com>
2022-03-09 11:27:07 +0530

    Java project has been created with simple implementation of MyInterface interface.

---
+++ Pack1/src/Demo.java
@@ -0,0 +1,11 @@
+//import Pack1.MyInterfaceImp;
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyInterfaceImp1 imp1 = new MyInterfaceImp1();
+		imp1.disp();
+		
+	}
+
+}
---
+++ Pack1/src/MyInterfaceImp1.java
@@ -0,0 +1,11 @@
+//package Pack1;
+public class MyInterfaceImp1 implements MyInterface
+{
+	
+	@Override
+	public void disp() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside MyInterfaceImp1 class");
+	}
+
+}
